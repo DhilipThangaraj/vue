5cf3d51d6a1bf955570f285459cd70fe2d8795ad
Dhilipkumar Thangaraj <dhilipthangaraj777@gmail.com>
2022-11-28 16:06:14 +0400

    button example

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,7 @@
 <!-- Basic -->
 <!-- 1.Hello World -->
 
-<script>
+<!-- <script>
 export default {
   data() {
     return {
@@ -15,4 +15,46 @@ export default {
   <h1>{{ message }}</h1>
 </template>
 
-<style></style>
+<style></style> -->
+
+<!--
+2.This example demonstrates handling user input with the v-on directive.
+-->
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World!",
+    };
+  },
+  methods: {
+    reverseMessage() {
+      this.message = this.message.split("").reverse().join("");
+    },
+  },
+  notify() {
+    alert("navigation was prevented");
+  },
+};
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <br />
+  <button @click="reverseMessage">Reverse The String</button>
+  <br />
+  <button @click="() => (message += '!')">Append "!"</button>
+  <br />
+  <a href="https://vuejs.org" @click.prevent="notify">
+    A link with e.preventDefault()
+  </a>
+</template>
+
+<style>
+button,
+a {
+  display: block;
+  margin-bottom: 1em;
+}
+</style>
