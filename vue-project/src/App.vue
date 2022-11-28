defcc56768b2c4f3b3fb01c1a0fc6fcf2831c592
Dhilipkumar Thangaraj <dhilipthangaraj777@gmail.com>
2022-11-28 16:44:27 +0400

    Attribute Binding

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -20,7 +20,7 @@ export default {
 <!--
 2.This example demonstrates handling user input with the v-on directive.
 -->
-
+<!-- 
 <script>
 export default {
   data() {
@@ -57,4 +57,54 @@ a {
   display: block;
   margin-bottom: 1em;
 }
-</style>
+</style> -->
+
+<!-- 3.Attribute binding -->
+<!--
+Here we are reactively binding element attributes / properties to the state.
+The :title syntax is short for v-bind:title.
+-->
+
+<!-- <script>
+export default {
+  data() {
+    return {
+      message: "Hello World!",
+      isRed: true,
+      color: "yellow",
+    };
+  },
+  methods: {
+    toggleRed() {
+      this.isRed = !this.isRed;
+    },
+    toggleColor() {
+      this.color = this.color === "green" ? "blue" : "green";
+    },
+  },
+};
+</script>
+
+<template>
+  <p>
+    <span :title="message">
+      Hover your mouse over me for a few seconds to see my dynamically bound
+      title!
+    </span>
+  </p>
+
+
+  <p :class="{ red: isRed }" @click="toggleRed">
+    This should be red... but click me to toggle it.
+  </p>
+
+  <p :style="{ color }" @click="toggleColor">
+    This should be green, and should toggle between green and blue on click.
+  </p>
+</template>
+
+<style>
+.red {
+  color: red;
+}
+</style> -->
