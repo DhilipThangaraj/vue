e5bd18e6f2870330339c6f36566550607c8891df
Dhilipkumar Thangaraj <dhilipthangaraj777@gmail.com>
2022-11-29 17:20:32 +0400

    different control methods

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -111,7 +111,7 @@ export default {
 
 <!-- 4. Conditional and loops -->
 
-<script>
+<!-- <script>
 export default {
   data() {
     return {
@@ -141,4 +141,67 @@ button {
   border: 1px solid;
   border-radius: 2px;
 }
-</style>
+</style> -->
+
+<!-- 5.Form Binding -->
+<script>
+export default {
+  data() {
+    return {
+      text: "Edit me",
+      checked: true,
+      checkedNames:["Jack"],
+      picked:"one",
+      selected:"A",
+      multiSelected: ['A']
+    };
+  },
+};
+</script>
+<template>
+  <h2>Text Input</h2>
+  <input v-model="text" />
+  <h2>Single Check Box</h2>
+  <input type="checkbox" id="checkbox" v-model="checked" />
+  <label for="checkbox">Checked : {{ checked }}</label>
+  <!--
+    multiple checkboxes can bind to the same
+    array v-model value
+  -->
+  <h2>Multi Check Box</h2>
+  <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+  <label for="jack">Jack</label>
+  <input type="checkbox" id="john" value="John" v-model="checkedNames">
+  <label for="john">John</label>
+  <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+  <label for="mike">Mike</label>
+  <p>Checked names: <pre>{{ checkedNames }}</pre></p>
+  <h2>Radio</h2>
+  <input type="radio" id="one" value="One" v-model="picked"/>
+  <label for="one">One</label>
+  <br/>
+  <input type="radio" id="two" value="Two" v-model="picked"/>
+  <label for="one">Two</label>
+  <br/>
+  <input type="radio" id="three" value="Three" v-model="picked"/>
+  <label for="one">Three</label>
+  <span>Picked: {{ picked }}</span>
+  <h2>Select</h2>
+  <select v-model="selected">
+   <option disabled value="">
+    Please select one
+   </option>
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+    <span>Selected: {{ selected }}</span>
+  </select>
+  <h2>Multi Select</h2>
+  <select v-model="multiSelected" multiple style="width:100px">
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+  <span>Selected: {{ multiSelected }}</span>
+</template>
+<style></style>
