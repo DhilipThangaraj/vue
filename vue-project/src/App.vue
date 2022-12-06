ea5929744d2f475bbaa2024fec080e94e797f140
Dhilipkumar Thangaraj <dhilipthangaraj777@gmail.com>
2022-12-06 13:23:11 +0400

    fetch api example, mount - initial call for the api,watch - for when updates happens on the state

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -295,3 +295,117 @@ code {
   color: #f66;
 }
 </style> -->
+
+<!-- computed and methods difference -->
+
+<!-- <script>
+export default {
+  data: () => ({
+    counterComputed: 0,
+    counterMethod: 0,
+  }),
+  computed: {
+    printTextComputed: function () {
+      console.log("counter printed from computed:", this.counterComputed);
+    },
+  },
+  methods: {
+    printTextMethod: function () {
+      console.log("counter printed from method:", this.counterMethod);
+    },
+  },
+};
+</script>
+<template>
+  <button @click="counterComputed++">computed button</button>
+  <p>{{ counterComputed }}</p>
+  <br />
+  <button @click="counterMethod++">method button</button>
+  <p>{{ counterMethod }}</p>
+  <br />
+  {{ printTextMethod() }}
+  {{ printTextComputed }}
+</template> -->
+
+<!-- 8. Fetch Data -->
+
+<!-- <script>
+const API_URL = `https://api.github.com/repos/vuejs/core/commits?per_page=3&sha=`;
+
+export default {
+  data: () => ({
+    branches: ["main", "v2-compat"],
+    currentBranch: "main",
+    commits: null,
+  }),
+
+  created() {
+    // fetch on init
+    console.log("==================created");
+    this.fetchData();
+  },
+
+  watch: {
+    // re-fetch whenever currentBranch changes
+    currentBranch: "fetchData",
+  },
+
+  methods: {
+    async fetchData() {
+      const url = `${API_URL}${this.currentBranch}`;
+      this.commits = await (await fetch(url)).json();
+    },
+    truncate(v) {
+      const newline = v.indexOf("\n");
+      return newline > 0 ? v.slice(0, newline) : v;
+    },
+    formatDate(v) {
+      return v.replace(/T|Z/g, " ");
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Latest Vue Core Commits</h1>
+  <template v-for="branch in branches">
+    <input
+      type="radio"
+      :id="branch"
+      :value="branch"
+      name="branch"
+      v-model="currentBranch"
+    />
+    <label :for="branch">{{ branch }}</label>
+  </template>
+  <p>vuejs/vue@{{ currentBranch }}</p>
+  <ul>
+    <li v-for="{ html_url, sha, author, commit } in commits">
+      <a :href="html_url" target="_blank" class="commit">{{
+        sha.slice(0, 7)
+      }}</a>
+      - <span class="message">{{ truncate(commit.message) }}</span
+      ><br />
+      by
+      <span class="author">
+        <a :href="author.html_url" target="_blank">{{ commit.author.name }}</a>
+      </span>
+      at <span class="date">{{ formatDate(commit.author.date) }}</span>
+    </li>
+  </ul>
+</template>
+
+<style>
+a {
+  text-decoration: none;
+  color: #42b883;
+}
+li {
+  line-height: 1.5em;
+  margin-bottom: 20px;
+}
+.author,
+.date {
+  font-weight: bold;
+}
+</style> -->
