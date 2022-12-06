1cd588105aa3f70530d8e2df11dc7d3e3c1ba2d9
Dhilipkumar Thangaraj <dhilipthangaraj777@gmail.com>
2022-12-06 17:47:28 +0400

    grid with sort and filter

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -409,3 +409,31 @@ li {
   font-weight: bold;
 }
 </style> -->
+<!-- 
+9.Grid with sort and filter -->
+<script>
+import DemoGrid from "./components/GridView.vue";
+
+export default {
+  components: {
+    DemoGrid,
+  },
+  data() {
+    return {
+      searchQuery: "",
+      gridColumns: ["name", "power"],
+      gridData: [
+        { name: "Chuck Norris", power: Infinity },
+        { name: "Bruce Lee", power: 9000 },
+        { name: "Jackie Chan", power: 7000 },
+        { name: "Jet Li", power: 8000 },
+      ],
+    };
+  },
+};
+</script>
+<template>
+  <form id="search">Search <input name="query" v-model="searchQuery" /></form>
+  <DemoGrid :data="gridData" :columns="gridColumns" :filter-key="searchQuery">
+  </DemoGrid>
+</template>
